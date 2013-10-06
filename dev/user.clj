c3fa5a5ee9aba80d6ccca4ba4734bff70267edde
Ronen Narkis <narkisr@gmail.com>
2013-10-06 20:59:02 +0300

    adding dev src folder

---
+++ dev/user.clj
@@ -0,0 +1,11 @@
+(ns user
+  (:use midje.repl)
+  (:require 
+     [clojure.tools.trace :refer (deftrace trace trace-ns trace-vars)]
+     [clojure.java.io :as io]
+     [clojure.string :as str]
+     [clojure.pprint :refer (pprint)]
+     [clojure.repl :refer :all]
+     [clojure.tools.namespace.repl :refer (refresh refresh-all)]
+    ))
+
