2092de2f527e6444813b4537557a30797dc1b92c
Ronen Narkis <narkisr@gmail.com>
2013-10-06 20:59:41 +0300

    spliting ids and core

--- src/puny/core.clj
+++ src/puny/core.clj
@@ -17,44 +17,18 @@
     [clojure.set :only (difference)]
     [slingshot.slingshot :only  [throw+ try+]]
     [puny.redis :only (wcar hsetall*)]
+    [robert.hooke :only (add-hook)]
     [clojure.core.strint :only (<<)])
   (:require 
+    [puny.ids :refer (<<< fn-ids id-modifiers indices-fn-ids ex bang-fn-ids)]
     [clojure.string :refer [split]]
-    [inflections.core :refer (plural)]
     [taoensso.carmine :as car]))
-
-(defmacro <<< 
-  "String interpulation into a symbol"
-  [s] `(symbol (<< ~s)))
-
-(defmacro <<k 
-  "String interpulation into a keyword"
-  [s] `(keyword (<< ~s)))
-
-(defn fn-ids [name*]
-  {:id-fn (<<< "~{name*}-id") :exists-fn (<<< "~{name*}-exists?")
-   :add-fn (<<< "add-~{name*}") :update-fn (<<< "update-~{name*}")
-   :validate-fn (<<< "validate-~{name*}") :gen-fn (<<< "gen-~{name*}-id") 
-   :delete-fn (<<< "delete-~{name*}") :get-fn (<<< "get-~{name*}")
-   :partial-fn (<<< "partial-~{name*}")
-   :all-fn (<<< "all-~(plural name*)")
-   })
-
-(defn id-modifiers [name* opts]
-  "update arguments and key fn matching id-prop existence"
-  (if-let [id-prop (opts :id)]
-     {:up-args (vector {:keys [id-prop] :as 'v}) :up-id id-prop :add-k-fn (list 'v (keyword id-prop))}
-     {:up-args ['id 'v] :up-id 'id :add-k-fn (list (:gen-fn (fn-ids name*)))}))
 
 (defmacro defgen 
   "An id generator" 
   [name*]
   `(defn ~(<<< "gen-~{name*}") []
     (wcar (car/incr ~(<< "~{name*}:ids")))))
-
-(defn indices-fn-ids [name*]
-  {:index-add (<<< "index-~{name*}") :index-del (<<< "clear-~{name*}-indices")
-   :index-get (<<< "get-~{name*}-index") :reindex (<<< "reindex-~{name*}") })
 
 (defn index-key 
   "index key format, composed of name index type and value: person:namejohn"
@@ -81,20 +55,6 @@
         (defn- ~reindex [~'id ~'old ~'new]
           (~index-del ~'id ~'old) 
           (~index-add ~'id ~'new)))))
-
-(defn ex 
-   "Creates an exception key" 
-   [name* type]
-   (<<k "~{*ns*}/~{type}-~{name*}"))  
-
-(defn bang-fn-ids [name*]
-  (let [{:keys [id-fn delete-fn get-fn exists-fn ]} (fn-ids name*)] 
-    {:missing-ex (ex name* "missing") 
-     :exists! (<<< "~{name*}-exists!")  
-     :delete! (<<< "delete-~{name*}!")
-     :get! (<<< "get-~{name*}!")
-     }))
-
 
 (defmacro bang-fns
   "A fail fast versions of read/delete functions (will fail if entity is missing), 
---
+++ src/puny/ids.clj
@@ -0,0 +1,48 @@
+(ns puny.ids
+  "Misc id names of all functions, args etc.."
+  (:require 
+    [clojure.core.strint :refer (<<)] 
+    [inflections.core :refer (plural)]
+    )
+ )
+
+(defmacro <<< 
+  "String interpulation into a symbol"
+  [s] `(symbol (<< ~s)))
+
+(defmacro <<k 
+  "String interpulation into a keyword"
+  [s] `(keyword (<< ~s)))
+
+(defn fn-ids [name*]
+  {:id-fn (<<< "~{name*}-id") :exists-fn (<<< "~{name*}-exists?")
+   :add-fn (<<< "add-~{name*}") :update-fn (<<< "update-~{name*}")
+   :validate-fn (<<< "validate-~{name*}") :gen-fn (<<< "gen-~{name*}-id") 
+   :delete-fn (<<< "delete-~{name*}") :get-fn (<<< "get-~{name*}")
+   :partial-fn (<<< "partial-~{name*}")
+   :all-fn (<<< "all-~(plural name*)")
+   })
+
+(defn id-modifiers [name* opts]
+  "update arguments and key fn matching id-prop existence"
+  (if-let [id-prop (opts :id)]
+     {:up-args (vector {:keys [id-prop] :as 'v}) :up-id id-prop :add-k-fn (list 'v (keyword id-prop))}
+     {:up-args ['id 'v] :up-id 'id :add-k-fn (list (:gen-fn (fn-ids name*)))}))
+ 
+(defn indices-fn-ids [name*]
+  {:index-add (<<< "index-~{name*}") :index-del (<<< "clear-~{name*}-indices")
+   :index-get (<<< "get-~{name*}-index") :reindex (<<< "reindex-~{name*}") })
+
+(defn ex 
+   "Creates an exception key" 
+   [name* type]
+   (<<k "~{*ns*}/~{type}-~{name*}"))  
+
+(defn bang-fn-ids [name*]
+  (let [{:keys [id-fn delete-fn get-fn exists-fn ]} (fn-ids name*)] 
+    {:missing-ex (ex name* "missing") 
+     :exists! (<<< "~{name*}-exists!")  
+     :delete! (<<< "delete-~{name*}!")
+     :get! (<<< "get-~{name*}!")
+     }))
+ 
