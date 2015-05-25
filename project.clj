b92c2607ab69ec554086ed574730490e29a2e728
ronen <narkisr@gmail.com>
2015-05-25 15:51:18 +0300

    latest versions

--- project.clj
+++ project.clj
@@ -2,31 +2,31 @@
   :description "A puny map to redis persistency layer"
   :url "https://github.com/narkisr/puny"
   :license  {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [robert/hooke "1.3.0"] 
-                 [org.clojure/clojure "1.5.1"]
-                 [com.taoensso/carmine "2.0.0"]
-                 [substantiation "0.0.7"]
-                 [org.flatland/useful "0.10.3"]
+                 [com.taoensso/carmine "2.10.0"]
+                 [substantiation "0.2.1"]
+                 [org.flatland/useful "0.11.3"]
                  [org.clojure/core.incubator "0.1.3"]
-                 [inflections "0.8.1"]]
+                 [inflections "0.9.14"]]
   
-  :plugins  [[jonase/eastwood "0.0.2"] [lein-midje "3.0.0"] [lein-ancient "0.4.2"]
+  :plugins  [[jonase/eastwood "0.0.2"] [lein-midje "3.1.3"] [lein-ancient "0.6.7"]
              [lein-tag "0.1.0"] [lein-set-version "0.3.0"]]
 
-  :aliases {"autotest"
-            ["midje" ":autotest" ":filter" "-integration"] 
-            "runtest"
-            ["midje" ":filter" "-integration"] 
-            }
-  :profiles {:dev {
-               :source-paths  ["dev"]
-               :dependencies [[org.clojure/tools.trace "0.7.5"] [midje "1.5.1"] [junit/junit "4.11"]]
-              
-              :set-version {
-                  :updates [ 
-                    {:path "README.md" :search-regex #"\"\d+\.\d+\.\d+\""}]}
-               }
-             }
+  :aliases {
+            "autotest" ["midje" ":autotest" ":filter" "-integration"] 
+            "runtest" ["midje" ":filter" "-integration"]
+           }
 
-  )
+  :profiles {
+    :dev {
+        :source-paths ["dev"]
+        :dependencies [[midje "1.6.3"] [org.clojure/tools.trace "0.7.8"]]
+              
+        :set-version {
+            :updates [ {:path "README.md" :search-regex #"\"\d+\.\d+\.\d+\""}]
+        }
+    }
+  }
+
+)
